f8a573a39d75a8cb1d2a266777ddc849da3a37fb
Glitch (react-rate-calculator-starting-point) <none>
2019-01-23 11:37:11 -0600

    Initial and potentially final commit. Whoops.

--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -6,14 +6,30 @@ import Output from './output';
 export default class RateCalculator extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            hours: 1,
+            rate: 1
+        }
+    }
+
+    changeDayRate(rate){
+        this.setState({
+            rate
+        })
+    }
+
+    changeHours(hours){
+        this.setState({
+            hours
+        })
     }
 
     render() {
-        const rate = 10;
+        const rate = this.state.rate / this.state.hours;
         return (
             <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
+                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} handleChange={(e) =>this.changeDayRate(e)}/>
+                <NumberInput id="hours" label="Hours" min={1} max={12} handleChange={(e) => this.changeHours(e)}/>
                 <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
 />
             </form>
